78e0d5fda605c3561711313e4d45a191c42e7489
Dave Kiss <dave@davekiss.com>
2022-06-15 13:31:26 -0400

    Add a huge recording progress bar

---
+++ components/Recorder/RecordingProgress.tsx
@@ -0,0 +1,19 @@
+import * as React from "react";
+import { useRecorderContext, RecordingStatus, RECORDING_DURATION } from "contexts/RecorderContext";
+import { motion } from "framer-motion"
+
+const RecordingProgress = () => {
+  const { recordingStatus, setRecordingStatus } = useRecorderContext();
+
+  if (recordingStatus !== RecordingStatus.RECORDING) return null;
+
+  return (
+    <motion.div
+      className="p-4 sm:p-16 absolute w-full text-center bg-red-700"
+      animate={{ x: "100%" }}
+      transition={{ duration: RECORDING_DURATION }}>
+    </motion.div>
+  )
+}
+
+export default RecordingProgress;
