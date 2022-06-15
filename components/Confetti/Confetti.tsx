108a2e3415d22cd0c5ec927d4c421c4a9d3e5b76
Dave Kiss <dave@davekiss.com>
2022-06-15 07:50:03 -0400

    Only run on recorder stop

--- components/Confetti/Confetti.tsx
+++ components/Confetti/Confetti.tsx
@@ -1,18 +1,23 @@
 import React from 'react';
 import Celebration from 'react-confetti';
 import { useWindowSize } from 'react-use';
+import { useRecorderContext, RecordingStatus } from "contexts/RecorderContext";
 
 const Confetti = () => {
   const { width, height } = useWindowSize();
   const [isClient, setClient] = React.useState(false);
 
-  const [run, shouldRun] = React.useState(true);
+  const { recordingStatus } = useRecorderContext();
+  const [run, shouldRun] = React.useState(false);
 
   React.useEffect(() => {
     setClient(true);
   }, []);
 
   React.useEffect(() => {
+    if (recordingStatus !== RecordingStatus.STOPPING) return;
+    shouldRun(true);
+
     const interval = setTimeout(() => {
       shouldRun(false);
     }, 5000);
@@ -20,7 +25,7 @@ const Confetti = () => {
     return () => {
       clearTimeout(interval);
     };
-  }, []);
+  }, [recordingStatus]);
 
   return (
     <>
