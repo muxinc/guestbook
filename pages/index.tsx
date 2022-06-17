a86aa98c73623e39f616dc9a880aa39fda53def9
Dave Kiss <dave@davekiss.com>
2022-06-17 07:58:42 -0400

    Update page meta

--- pages/index.tsx
+++ pages/index.tsx
@@ -10,14 +10,14 @@ const Home: NextPage = () => {
   return (
     <>
       <Head>
-        <title>Mux Video Guestbook | React Miami</title>
+        <title>Mux Video Guestbook</title>
         <meta
           name="description"
-          content="For all those good React Miami memories, from your good friends at Mux."
+          content="For all those good memories, from your friends at Mux."
         />
         <link
           rel="icon"
-          href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>🌴</text></svg>"
+          href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>📖</text></svg>"
         />
       </Head>
       <main className="h-[100vh] h-[100svh] flex flex-col">
