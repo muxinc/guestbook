d3ad5714182125116cfb93f67a499a55d1891fe3
Dave Kiss <dave@davekiss.com>
2022-06-17 11:54:49 -0400

    Build out some sharing options

--- pages/entry/[id].tsx
+++ pages/entry/[id].tsx
@@ -1,8 +1,8 @@
+import * as React from "react";
 import { supabase } from "../../utils/supabaseClient";
 import type { GetStaticPaths, GetStaticProps, NextPage } from 'next';
 
 import Head from "next/head";
-import { useRouter } from 'next/router'
 
 import MuxVideo from "@mux-elements/mux-video-react";
 import Navbar from "../../components/Navbar";
@@ -17,19 +17,51 @@ type Props = {
 };
 
 const Entry: NextPage<Props> = ({ entry: { playback_id } }) => {
+
+  const shareData = React.useMemo(() => ({
+    title: 'AWS Summit NYC',
+    text: 'OMG! We had so much fun at AWS Summit NYC!',
+    url: document.location.href,
+  }), []);
+
+  const shareIt = React.useCallback(() => {
+    if (navigator.share) {
+      navigator.share(shareData)
+        .then(() => console.log('Successful share'))
+        .catch((error) => console.log('Error sharing', error));
+    }
+  }, [shareData]);
+
   return (
     <>
       <Head>
-        <title>Mux Video Guestbook | React Miami</title>
+        <title key="title">Mux Video Guestbook</title>
         <meta
+          key="description"
           name="description"
-          content="For all those good React Miami memories, from your good friends at Mux."
+          content="For all those good memories, from your friends at Mux."
         />
         <link
           rel="icon"
-          href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>🌴</text></svg>"
+          href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>📖</text></svg>"
         />
+
+        <meta key="twitter:card" name="twitter:card" content="summary_large_image" />
+        <meta key="twitter:site" name="twitter:site" content="@MuxHQ" />
+
+        <meta key="og:site_name" property="og:site_name" content="Mux Video Guestbook" />
+        <meta key="og:url" property="og:url" content={`https://guestbook.mux.dev`} />
+        <meta key="og:type" property="og:type" content="website" />
+        <meta key="og:title" property="og:title" content="Mux Video Guestbook" />
+        <meta key="og:description" property="og:description" content="For all those good memories, from your friends at Mux." />
+        <meta
+          key="og:image"
+          property="og:image"
+          content="https://mux.com/files/mux-video-logo-square.png"
+        />
+        <meta key="og:locale" property="og:locale" content="en_US" />
       </Head>
+
       <main className="h-[100vh] h-[100svh] flex flex-col">
         <Navbar />
         <MuxVideo
@@ -45,6 +77,16 @@ const Entry: NextPage<Props> = ({ entry: { playback_id } }) => {
           muted
           loop
         />
+        <div>
+          <a
+            href={`https://twitter.com/share?text=${shareData.text}&url=${shareData.url}`}
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Tweet
+          </a>
+          {navigator?.canShare && navigator.canShare(shareData) && <div onClick={shareIt}>Share</div>}
+        </div>
       </main>
     </>
   );
