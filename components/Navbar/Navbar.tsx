f209c226fcd335dcbb546daf5ccd2669c03e5c30
Dave Kiss <dkiss@mux.com>
2022-08-26 11:01:34 -0400

    Add utm params to navbar

--- components/Navbar/Navbar.tsx
+++ components/Navbar/Navbar.tsx
@@ -9,7 +9,7 @@ type Props = {
 const Navbar = ({ withSettings = true, subheading = "" }: Props) => (
   <div className="p-4 sm:p-8 relative text-center">
     <h1>
-      <a href="https://mux.com">
+      <a href="https://mux.com?utm_source=mux-guestbook&utm_medium=conference&utm_campaign=cascadiajs">
         <Mux className="inline-block h-7 sm:h-10 w-auto mb-2" />
       </a>
       <div className="text-xl">{subheading}</div>
