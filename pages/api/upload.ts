fb3e3f4112f25ffeb70e9642f63d9c48f142a580
Dave Kiss <dkiss@mux.com>
2022-08-31 10:29:55 -0700

    Provide delete_key in upload api response

--- pages/api/upload.ts
+++ pages/api/upload.ts
@@ -7,6 +7,7 @@ const Mux = require("@mux/mux-node").default;
 type Data = {
   id: number | null;
   url: string;
+  delete_key: string;
 };
 
 export default async function handler(
@@ -59,5 +60,6 @@ export default async function handler(
   res.status(201).json({
     id: data ? data[0].id : null,
     url: upload.url,
+    delete_key: upload.id
   });
 }
